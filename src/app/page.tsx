e051c0ff20704641b30b7768e7262aa249a782de
Grace <27grace@gmail.com>
2024-02-24 23:23:40 +0000

    Update to use Nottingham SVG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,27 @@
-import { Card, CardBody, CardHeader } from "@chakra-ui/card";
-import { Heading, Image, VStack, Text, AspectRatio } from "@chakra-ui/react";
+import { Heading, Image, VStack, Text } from "@chakra-ui/react";
 
 export default function SaveTheDateApp() {
   return (
-    <main className="flex flex-col items-center justify-center h-full">
-      <VStack p={5}>
-        <Card>
-          <CardHeader pb={0}>
-            <Heading>Save the date</Heading>
-          </CardHeader>
-          <CardBody>
-            {/* Image to be replaced */}
-            <Image
-              maxW={500}
-              rounded="sm"
-              objectFit="cover"
-              src="/save-the-date.jpeg"
-              alt="Dan and Grace smiling and hugging each other sweetly."
-            />
-            <VStack m={5}>
-              <Text fontSize="2xl">Dan & Grace</Text>
-              <VStack fontSize="xl" gap={0}>
-                <Text>10th May 2023</Text>
-                <Text>Carlton, Nottingham, UK</Text>
-              </VStack>
-              <Text>More information to come!</Text>
-            </VStack>
-          </CardBody>
-        </Card>
+    <main className="flex flex-col items-center justify-center h-full overflow-hidden">
+      <VStack p={5} w="100vw" textAlign="center">
+        <Heading>Save the date</Heading>
+        <Image
+          maxW={700}
+          rounded="sm"
+          objectFit="cover"
+          src="save_the_date_nottingham.svg"
+          alt="Drawing of Nottingham notable landmarks, such as the Robin Hood statue and the Nottingham Council House."
+        />
+        <VStack>
+          <Text fontSize="2xl" fontWeight={500}>
+            Dan & Grace
+          </Text>
+          <VStack fontSize="xl" gap={0}>
+            <Text>10th May 2025</Text>
+            <Text>Carlton, Nottingham, UK</Text>
+          </VStack>
+          <Text>Invite and updates on this website to come!</Text>
+        </VStack>
       </VStack>
     </main>
   );

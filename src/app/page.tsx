f2cad983783d9146310ebcd219eafab226d29207
Grace <27grace@gmail.com>
2024-02-25 00:07:46 +0000

    Update to use Raleway font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Heading, Image, VStack, Text } from "@chakra-ui/react";
 
 export default function SaveTheDateApp() {
   return (
-    <main className="flex flex-col items-center justify-center h-full overflow-hidden">
+    <main className="flex flex-col items-center justify-center h-full overflow-hidden bg-#f9f9f9">
       <VStack p={5} w="100vw" textAlign="center">
-        <Heading>Save the date</Heading>
+        <Heading fontWeight={700}>Save the date</Heading>
         <Image
           maxW={700}
           rounded="sm"
@@ -12,8 +12,8 @@ export default function SaveTheDateApp() {
           src="save_the_date_nottingham.svg"
           alt="Drawing of Nottingham notable landmarks, such as the Robin Hood statue and the Nottingham Council House."
         />
-        <VStack>
-          <Text fontSize="2xl" fontWeight={500}>
+        <VStack m={5} fontWeight={400}>
+          <Text fontSize="2xl" fontWeight={600}>
             Dan & Grace
           </Text>
           <VStack fontSize="xl" gap={0}>

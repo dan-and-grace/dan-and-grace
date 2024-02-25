49e4a83a6505b0b33c543b85b22b017b92b329b2
Grace <27grace@gmail.com>
2024-02-25 08:56:42 +0000

    Tweak text arrangement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,27 @@ import { Heading, Image, VStack, Text } from "@chakra-ui/react";
 
 export default function SaveTheDateApp() {
   return (
-    <main className="flex flex-col items-center justify-center h-full overflow-hidden bg-#f9f9f9">
-      <VStack p={5} w="100vw" textAlign="center">
-        <Heading fontWeight={700}>Save the date</Heading>
+    <main className="flex flex-col items-center justify-center h-full overflow-hidden">
+      <VStack p={3} w="100vw" textAlign="center">
         <Image
+          maxH="50vh"
           maxW={700}
           rounded="sm"
           objectFit="cover"
           src="save_the_date_nottingham.svg"
           alt="Drawing of Nottingham notable landmarks, such as the Robin Hood statue and the Nottingham Council House."
         />
-        <VStack m={5} fontWeight={400}>
-          <Text fontSize="2xl" fontWeight={600}>
-            Dan & Grace
+        <VStack mt={5} mb={5} fontWeight={400} gap={0}>
+          <Heading fontSize="3xl" fontWeight={700}>
+            SAVE THE DATE
+          </Heading>
+          <Text fontSize="3xl" fontWeight={600}>
+            10th May 2025
           </Text>
-          <VStack fontSize="xl" gap={0}>
-            <Text>10th May 2025</Text>
-            <Text>Carlton, Nottingham, UK</Text>
-          </VStack>
-          <Text>Invite and updates on this website to come!</Text>
+          <Text fontFamily="Pacifico">for the wedding of</Text>
+          <Text fontSize="2xl">Dan Bartley & Grace Chin</Text>
+          <Text fontSize="xl">Carlton, Nottingham, UK</Text>
+          <Text>Invite & updates on this website to come!</Text>
         </VStack>
       </VStack>
     </main>
--- theme.ts
+++ theme.ts
@@ -2,7 +2,10 @@ import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
     fonts: {
-      heading: 'raleway, sans-serif',
-      body: 'raleway, sans-serif',
+      heading: 'Gabarito, DM Serif Display, Playfair Display, sans-serif',
+      body: 'Gabarito',
     }
 });
+// Cursive: Satisfy, Pacifico
+// Simple: Gabarito, Raleway
+// Fancy: EB Garamond, DM Serif Display, Playfair Display
